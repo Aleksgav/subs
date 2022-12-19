50c2353d7839497612d02ed4ed2824677c0dec59
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2022-12-19 08:27:00 +0300

    [feat]: apply default formatting

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use subs::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
 
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,4 @@
 use std::net::TcpListener;
-use reqwest;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -16,8 +15,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
 
     let server = subs::run(listener).expect("Failed to bind address");
