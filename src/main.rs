e2f8d25dad1e51c900c4fd2bdcdf2dcb378c8b10
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2022-12-21 10:05:28 +0300

    [feat]: added initial configuration

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@
 use std::net::TcpListener;
 
 use subs::startup::run;
+use subs::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
 }
