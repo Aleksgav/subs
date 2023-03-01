504ec905b0b002dac873bb256f32899a0f017290
Aleksandr Gavrishchuk <alex.phyp@gmail.com>
2023-03-01 23:39:38 +0300

    [feat]: set connection timeout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use sqlx::PgPool;
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
-use secrecy::ExposeSecret;
 use subs::configuration::get_configuration;
 use subs::startup::run;
 use subs::telemetry::{get_subscriber, init_subsciriber};
@@ -14,9 +14,10 @@ async fn main() -> std::io::Result<()> {
     init_subsciriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port,
